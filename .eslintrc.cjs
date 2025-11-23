edbccc87563e595b6fc4ad7b6b361cae9471014c
Bill Han <bill.han@l3harris.com>
2025-11-22 22:13:35 -0500

    chore(ui): add ESLint configuration to fix pipeline lint failure

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/* ESLint configuration for rfp-match-ui */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'react/prop-types': 'off', // using TypeScript types or modern patterns
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/'],
+};
